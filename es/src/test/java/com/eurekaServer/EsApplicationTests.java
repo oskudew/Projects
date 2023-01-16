c6b5d5c5145a7775c9ef313ab9f477c5eda33858
oskudew <65346563+oskudew@users.noreply.github.com>
2023-01-16 13:38:19 +0530

    Eureka server

---
+++ es/src/test/java/com/eurekaServer/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
