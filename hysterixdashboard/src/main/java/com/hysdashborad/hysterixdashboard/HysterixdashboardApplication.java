2620e09f5ce1424e0f2c2d5106cef54c2cd3929a
Oshank <oshudewangan@gmail.com>
2023-01-18 15:53:22 +0530

    added hysterix dashboard

---
+++ hysterixdashboard/src/main/java/com/hysdashborad/hysterixdashboard/HysterixdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.hysdashborad.hysterixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HysterixdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HysterixdashboardApplication.class, args);
+	}
+
+}
---
+++ hysterixdashboard/src/test/java/com/hysdashborad/hysterixdashboard/HysterixdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hysdashborad.hysterixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HysterixdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
