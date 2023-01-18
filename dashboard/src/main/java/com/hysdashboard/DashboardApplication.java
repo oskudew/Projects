9e257a2925400eebd5d7e39d921205bea0a05781
Oshank <oshudewangan@gmail.com>
2023-01-18 23:55:14 +0530

    revert hysterixDashboard1

--- dashboard/src/main/java/com/hysdashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/hysdashboard/DashboardApplication.java
@@ -2,8 +2,12 @@ package com.hysdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
 public class DashboardApplication {
 
 	public static void main(String[] args) {
