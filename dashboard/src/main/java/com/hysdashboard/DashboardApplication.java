fbf1e9f08e63869753d65f5258793b62ad8d281e
Oshank <oshudewangan@gmail.com>
2023-01-18 23:40:28 +0530

    update hysterix dashboard

---
+++ dashboard/src/main/java/com/hysdashboard/DashboardApplication.java
@@ -0,0 +1,13 @@
+package com.hysdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApplication.class, args);
+	}
+
+}
--- hysterixdashboard/src/main/java/com/hysdashborad/hysterixdashboard/HysterixdashboardApplication.java
+++ hysterixdashboard/src/main/java/com/hysdashborad/hysterixdashboard/HysterixdashboardApplication.java
@@ -2,8 +2,12 @@ package com.hysdashborad.hysterixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
 public class HysterixdashboardApplication {
 
 	public static void main(String[] args) {
